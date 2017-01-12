42d64b71db7a21355132d1c1573e12798e377b4c
Kishan Patel <kpatel20538@gmail.com>
2017-01-11 21:22:46 -0600

    PythagTree: Gather Squares & Triangles Implemented

--- incomplete/pythagoras.py
+++ incomplete/pythagoras.py
@@ -13,8 +13,20 @@ def gather_squares_triangles(p1,p2,depth)
     triangles [(float,float,float)...] : absolute positions of 
       vertices of right triangles
   """
-  pass
+  if depth == 0:
+    return [],[]
   
+  pd = (p2[0] - p1[0]),(p1[1] - p2[1]) 
+  p3 = (p2[0] - pd[1]),(p2[1] - pd[0])
+  p4 = (p1[0] - pd[1]),(p1[1] - pd[0])
+  p5 = (p4[0] + (pd[0] - pd[1])/2),(p4[1] - (pd[0] + pd[1])/2)
+  
+  squares_left,triangles_left = gather_squares_triangles(p4,p5,depth-1)
+  squares_right,triangles_right = gather_squares_triangles(p5,p3,depth-1)
+
+  squares = [[p1,p2,p3,p4]]+squares_left+squares_right
+  triangles = [[p3,p4,p5]]+triangles_left+triangles_right
+  return squares,triangles
 
 def task(argv):
   """ Draw a Depth-7 Pytagoras Tree without the use of Trig Functions """

043ddee3fb5c478991f5e531a5706e34124c2366
Kishan Patel <kpatel20538@gmail.com>
2017-01-11 18:35:14 -0600

    Planning Chaos Game Task

--- incomplete/chaos.py
+++ incomplete/chaos.py
@@ -2,7 +2,12 @@ import sys
 
 def task(argv):
   """ Task Description """
-  print("Hello World",argv)
+  # Initialize Canvas
+  # Calculate Location of Reference Points (Equilateral Triangle)
+  # Pick a random point in bounded by the Reference Points
+  # Loop Chaotic Procedure until timeout
+  # Draw Generated Points and Boundary Lines
+  # Commit Canvas
   return 0
   
 if __name__ == "__main__":
